913122e5ec51167b01f78d59f0f7643c1e569efa
Ali Muhammad Farooq <ali@Alis-iMac-Pro.local>
2022-10-24 04:40:04 +0500

    nprogress added

--- customer/src/pages/_app.tsx
+++ customer/src/pages/_app.tsx
@@ -1,10 +1,17 @@
 // src/pages/_app.tsx
 import "../styles/globals.css";
+import "../styles/nprogress.css";
+import nprogress from "nprogress";
 import { SessionProvider } from "next-auth/react";
 import type { Session } from "next-auth";
 import type { AppType } from "next/app";
 import { trpc } from "../utils/trpc";
 import Layout from "../layouts/layout";
+import Router from "next/router";
+
+Router.events.on("routeChangeStart", nprogress.start);
+Router.events.on("routeChangeComplete", nprogress.done);
+Router.events.on("routeChangeError", nprogress.done);
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
